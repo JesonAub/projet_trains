8716429b9ee24dd8f82f3748afab7ef9b9626ebe
JesonAub <149049301+JesonAub@users.noreply.github.com>
2024-04-10 22:21:01 +0200

    Delete tp_projet_trains_aubry_jeson directory

--- tp_projet_trains_aubry_jeson/database.py
+++
@@ -1,7 +0,0 @@
-from trains.models import Trains
-
-Trains(1, "Le Havre").save()
-Trains(2, "Rouen").save()
-Trains(3, "Evreux").save()
-Trains(4, "Cherbourg").save()
-Trains(5, "Caen").save()
--- tp_projet_trains_aubry_jeson/trains/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from trains.models import Trains
-
-admin.site.register(Trains)
-
-# Register your models here.
